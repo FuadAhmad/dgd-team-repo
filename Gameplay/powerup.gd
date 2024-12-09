edf47ce26dce43325f631bbfecb7b2b6af277ffd
Fuad Ahmad <fuadahmad01@gmail.com>
2024-12-09 13:33:09 -0700

    powerups adding

---
+++ Gameplay/powerup.gd
@@ -0,0 +1,63 @@
+extends HBoxContainer
+
+enum PStatus {
+	NONE,
+	NEXT,
+	READY,
+	STOP
+}
+enum PType {
+	SINGLE,
+	DOUBLE,
+	DAUB,
+	INSTANT,
+	FREEZE
+}
+@export var power_status : int
+var power_type 
+
+	
+func _ready():
+	var parent_node = find_node_in_parents("Powerups")
+	if parent_node:
+		print("Parent node found:", parent_node)
+	else:
+		print("Parent node not found")
+
+func find_node_in_parents(node_name):
+	var current_node = self
+	while current_node:
+		if current_node.name == node_name:
+			return current_node
+		current_node = current_node.get_parent()
+	return null
+	
+func setPowerup(status, type):
+	power_status = status
+	power_type = type
+	set_UI(status, type)
+	
+func set_UI(status, type):
+	if status == 0:
+		$TextureRect.texture = load("res://assets/powerups/arrow.png")
+	elif status == 1:
+		$TextureRect.texture = load("res://assets/powerups/ok.png")
+	elif status == 2:
+		$TextureRect.texture = load("res://assets/powerups/Close.png")
+		
+	if type == PType.SINGLE:
+		$TextureButton.texture = load("res://assets/powerups/SDaub.png")
+	elif type == PType.DOUBLE:
+		$TextureButton.texture = load("res://assets/powerups/DDaub.png")
+	elif type == PType.DAUB:
+		$TextureButton.texture = load("res://assets/powerups/Dab2.png")
+	elif type == PType.INSTANT:
+		$TextureButton.texture = load("res://assets/powerups/InstantBingo.png")
+	elif type == PType.FREEZE:
+		$TextureButton.texture = load("res://assets/powerups/DDaub.png")
+
+
+func _on_texture_button_pressed() -> void:
+	print("-----powerup status: " + str(power_status))
+	if power_status == 1:
+		print("-----powerup being used.")
---
+++ Gameplay/powerupManager.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+@onready var powerup_container: VBoxContainer = $VBoxContainer
+
+@export var powerup:PackedScene
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var p1 = powerup.instantiate()
+	var ps = p1.get_script()
+	
+	ps.setPowerup(2, 2)
+	powerup_container.add_child(p1)
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
