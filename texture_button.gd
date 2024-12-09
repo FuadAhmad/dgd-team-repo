ce6777812a15814c78664af89e7fe7d8d231deca
Fuad Ahmad <fuadahmad01@gmail.com>
2024-12-08 17:11:39 -0700

    loby n card selection

--- texture_button.gd
+++ texture_button.gd
@@ -13,3 +13,7 @@ func _process(delta: float) -> void:
 
 func _on_gui_input(event: InputEvent) -> void:
 	get_tree().change_scene_to_file( "res://Gameplay/gameplay.tscn")
+
+
+func _on_pressed() -> void:
+	get_tree().change_scene_to_file( "res://Gameplay/gameplay.tscn")
